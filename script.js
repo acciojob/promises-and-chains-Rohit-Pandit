89cf53de15c9485e73b1a4e87bc0664490ec5db6
Rohit-Pandit <159272518+Rohit-Pandit@users.noreply.github.com>
2025-05-16 11:33:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+const btn = document.getElementById("btn");
+btn.addEventListener('click',()=>{
+
+	const name = document.getElementById("name").value.trim();
+	const age = document.getElementById("age").value.trim();
+
+	if(name==="" || age===""){
+		alert("Please enter valid details.")
+	}
+	else{
+		if(age >= 18){
+			setTimeOut(4000,()=>{
+				alert(`Welcome, ${name}. You can vote.`)
+			})
+		}
+		else{
+			setTimeOut(4000,()=>{
+				alert(`"Oh sorry ${name}. You aren't old enough.`)
+			}
+		}
+	}
+	
+})
